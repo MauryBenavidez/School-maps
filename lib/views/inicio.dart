d6a8c9957edb97868f2f340b6a01ea12ed7e911a
LeonardoGalindez <leonardogalindez2018@gmail.com>
2021-08-07 19:29:06 -0300

    diseñando barra de búsqueda
    diseño

--- lib/views/inicio.dart
+++ lib/views/inicio.dart
@@ -1,120 +1,83 @@
-import 'dart:html';
+
+import 'dart:developer';
 
 import 'package:flutter/material.dart';
 
-
-
 class Inicio extends StatelessWidget {
+
   static String id = 'inicio';
   @override
   Widget build(BuildContext context) {
+    
+
     return Scaffold(
-         body: SafeArea(
-          child: _bodyPage(context),
-        ),  
-    );
-  }
-
-  Widget _bodyPage(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      mainAxisSize: MainAxisSize.max,
-      children: [
-       Padding(
-         padding: EdgeInsets.only(
-           top: 16.0,
-           left: 16.0,
-           right: 16.0,
-           ),
-           child: _appBar(),
-           ),
-           _body(),
-           
-       ],
-    );
-  }
-  
-
-  Widget _appBar() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        GestureDetector(
-          child: Icon(Icons.account_circle),
-          
-          onTap: (){
-            print('Hola');
-          },
-        ),
+      appBar: AppBar(
+        backgroundColor: Colors.cyan,
+        titleSpacing:15,
+        title: Text('School Maps'),
         
-        Column(
-          mainAxisSize: MainAxisSize.max,
-          children: [
-           
-            Row(
-              children: [
-                Text(
-                  'Schools ',
-                  style: TextStyle(
-                    fontSize: 19.0,
-                  )
-                  ),
-                Text(
-                  'Maps',
-                  style: TextStyle(
-                    fontSize: 19.0,
-                    
-                  )
-                  
-                  ),
-              ],
-            )
+        
+
+      ),
+      
+      body: Container(
+        decoration: new BoxDecoration(
+                         border: Border.all (color: Colors.grey, width: 1.0), // Una capa de borde gris
+            color: Colors.white,
+            borderRadius: BorderRadius.all( Radius.circular(10.0)),
+          ),
+        
+        child: Column(
+          
+          children: <Widget>[
+            TextField(
+              decoration: InputDecoration(hintText: "Busca aquí"),
+             
+                
+              
+              
+            ),
             
           ],
-        )
-      ],
+          
+        ),
+        
+      ),
+      
+           
     );
   }
 
-  
-  
-}
+  Widget app(BuildContext context){
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              '$Counter',
+              style: Theme.of(context).textTheme.bodyText1,
+            )
 
+          ],
+        ),
+      ),
+      floatingActionButton: new FloatingActionButton(
+        
+        child: Icon(Icons.account_circle),
+      ),
 
-Widget _body() {
-  final double _borderValue =35.0;
-  return Expanded(
-    child: ListView(
-      padding: EdgeInsets.only(top: 40.0),
-      shrinkWrap: true,
-      children: [
-        Container(
-          height: 60.0,
-          decoration: BoxDecoration(
-            color: Colors.cyan,
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(_borderValue),
-            topRight: Radius.circular(_borderValue),
-          ) 
-          ),
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
-
-            children: [
-              Padding(padding: EdgeInsets.only(
-                top: 40.0,
-                left: 40.0,
-                right: 30.0,
-              ) 
-
-              ),
-
-            ],
-          ),
-        )
-      ],
-    ),
     );
+      
+    
+  }
+
+  
+
+  
+  
+  
+
 }
 
 
