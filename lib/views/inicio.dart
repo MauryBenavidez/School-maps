90f5162883b0e86b53d1d16dae35ff158345df6b
LeonardoGalindez <leonardogalindez2018@gmail.com>
2021-08-14 19:24:41 -0300

    pantalla noticias
    Diseñando  pantalla

--- lib/views/inicio.dart
+++ lib/views/inicio.dart
@@ -1,8 +1,8 @@
-import 'package:flutter/material.dart';
-import 'package:schools_maps/views/widgets/barra.dart';
+import 'package:flutter/material.dart';//paquete principal
+import 'package:schools_maps/views/widgets/barra.dart';//pauquete de widget barra
 
 class Inicio extends StatefulWidget {
-  static String id ='Inicio';
+  static String id ='Inicio';//id de conexion con main
   @override
   State<Inicio> createState() => _MyStatefulWidgetState();
   
@@ -15,7 +15,7 @@ class _MyStatefulWidgetState extends State<Inicio> {
  
   void _onItemTapped(int index) {
     setState(() {
-      _selectedIndex = index;
+      _selectedIndex = index;//para que en la barra de navegacion cuando le de click a alugun boton este cambie de pantalla
     });
   }
 
@@ -28,10 +28,10 @@ class _MyStatefulWidgetState extends State<Inicio> {
         child: Column(
           mainAxisSize: MainAxisSize.max,
           children: [
-            Padding(padding: EdgeInsets.only(
+            Padding(padding: EdgeInsets.only(//para dar el tamaño a la barra de busqueda
 
             ),
-            child: Barra(),
+            child: Barra(),//conexión de widget barra con inicio
             )
           ],
         ),
@@ -40,12 +40,12 @@ class _MyStatefulWidgetState extends State<Inicio> {
       
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
+          BottomNavigationBarItem(//boton de inicio en la barra de busqueda, este nos permite ir de la pantalla de noticias a la de inicio
             icon: Icon(Icons.home),
             label: 'inicio',
             backgroundColor: Colors.red,
           ),
-          BottomNavigationBarItem(
+          BottomNavigationBarItem(//boton de noticias en la barra de navegacion, este nos permite ir del inicio a la pantalla de noticias
             icon: Icon(Icons.notifications),
             label: 'Noticias',
             backgroundColor: Colors.green,
@@ -53,8 +53,8 @@ class _MyStatefulWidgetState extends State<Inicio> {
           
         ],
         currentIndex: _selectedIndex,
-        selectedItemColor: Colors.blue,
-        onTap: _onItemTapped,
+        selectedItemColor: Colors.blue,//color de los botones cundo le doy click
+        onTap: _onItemTapped,//lo que pasa cuando le doy click
       ),
       
     );
