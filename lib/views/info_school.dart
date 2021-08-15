cbc22d154236d320b54cec5cc145d285838cca64
Maugit03 <mauricio.benavidez.epet20@gmail.com>
2021-08-15 10:24:02 -0300

    Pantalla Info Escuela - Añadiendo titulo
    Se le añadió el titulo de la escuela a la pantalla.

--- lib/views/info_school.dart
+++ lib/views/info_school.dart
@@ -14,7 +14,39 @@ class ViewInfoSchool extends State<InfoSchool> {
         title: Text('Información de la escuela'),
         centerTitle: true,
       ),
-      backgroundColor: (Color(0xFF1F5BA0)),
+      backgroundColor: (Color(0xFFFFFFFF)),
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 20.0,
+            ),
+            Text(
+              "Escuela Provincial de Educación Técnica N°20".toUpperCase(),
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 15,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'Impact',
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              "Centro de educación secundaria",
+              textAlign: TextAlign.start,
+              style: TextStyle(
+                color: Colors.grey,
+                fontSize: 15,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'Impact',
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
