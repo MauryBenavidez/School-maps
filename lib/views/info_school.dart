a6e047299cfbd90cf3a7f1de8c6cfdcf58e77fe2
Maugit03 <mauricio.benavidez.epet20@gmail.com>
2021-08-15 11:13:55 -0300

    Pantalla Info Escuela - Añadiendo Navigation Bar
    Se agregó el Bottom Navigation Bar.

--- lib/views/info_school.dart
+++ lib/views/info_school.dart
@@ -7,6 +7,7 @@ class InfoSchool extends StatefulWidget {
 }
 
 class ViewInfoSchool extends State<InfoSchool> {
+  int _pantallaActual = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,53 +16,80 @@ class ViewInfoSchool extends State<InfoSchool> {
         centerTitle: true,
       ),
       backgroundColor: (Color(0xFFFFFFFF)),
-      body: Center(
-        child: Column(
-          children: [
-            SizedBox(
-              height: 20.0,
-            ),
-            Text(
-              "Escuela Provincial de Educación Técnica N°20".toUpperCase(),
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                fontFamily: 'Impact',
-              ),
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            Text(
-              "Centro de educación secundaria",
-              textAlign: TextAlign.start,
-              style: TextStyle(
-                color: Colors.grey,
-                fontSize: 17,
-                fontWeight: FontWeight.bold,
-                fontFamily: 'Impact',
-              ),
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            Container(
-              child: Image.asset(
-                'assets/epet20.jpg',
-                height: 274,
-              ),
-              decoration: BoxDecoration(color: Colors.white, boxShadow: [
-                BoxShadow(
-                  offset: Offset(0, 10),
-                  blurRadius: 30,
-                ),
-              ]),
-            ),
-          ],
-        ),
+      body: _pantallaActual == 0 ? PantallaHome() : PantallaFotos(),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (index) {
+          setState(() {
+            _pantallaActual = index;
+          });
+        },
+        currentIndex: _pantallaActual,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Escuela"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.burst_mode_sharp), label: "Fotos")
+        ],
       ),
     );
+  }
+}
+
+class PantallaHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          SizedBox(
+            height: 20.0,
+          ),
+          Text(
+            "Escuela Provincial de Educación Técnica N°20".toUpperCase(),
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              fontFamily: 'Impact',
+            ),
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          Text(
+            "Centro de educación secundaria",
+            textAlign: TextAlign.start,
+            style: TextStyle(
+              color: Colors.grey,
+              fontSize: 17,
+              fontWeight: FontWeight.bold,
+              fontFamily: 'Impact',
+            ),
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          Container(
+            child: Image.asset(
+              'assets/epet20.jpg',
+              height: 274,
+            ),
+            decoration: BoxDecoration(color: Colors.white, boxShadow: [
+              BoxShadow(
+                offset: Offset(0, 10),
+                blurRadius: 30,
+              ),
+            ]),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class PantallaFotos extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
