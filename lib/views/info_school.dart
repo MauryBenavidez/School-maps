48921a5198ef32af93afd31bb777ecdb0347bef0
Maugit03 <mauricio.benavidez.epet20@gmail.com>
2021-08-15 13:16:10 -0300

    Pantalla Info Escuela - Agregando información
    Se agregó la información de la escuela EPET N°20.

--- lib/views/info_school.dart
+++ lib/views/info_school.dart
@@ -16,20 +16,27 @@ class ViewInfoSchool extends State<InfoSchool> {
         centerTitle: true,
       ),
       backgroundColor: (Color(0xFFFFFFFF)), //Color del fondo de la pantalla.
-      body: _pantallaActual == 0 ? PantallaHome() : PantallaFotos(), //Con esto se intercambia de una pantalla a otra.
-      bottomNavigationBar: BottomNavigationBar( //Esta es la barra de navegación que esta abajo de la pantalla.
-        type: BottomNavigationBarType.fixed, 
-        backgroundColor: Colors.blue, //Color del fondo de la barra de navegación.
+      body: _pantallaActual == 0
+          ? PantallaHome()
+          : PantallaFotos(), //Con esto se intercambia de una pantalla a otra.
+      bottomNavigationBar: BottomNavigationBar(
+        //Esta es la barra de navegación que esta abajo de la pantalla.
+        type: BottomNavigationBarType.fixed,
+        backgroundColor:
+            Colors.blue, //Color del fondo de la barra de navegación.
         selectedItemColor: Colors.white, //Color del item seleccionado.
         unselectedItemColor: Colors.black, //Color del item NO seleccionado.
         iconSize: 27, //Tamaño de los iconos de la barra de navegación.
-        onTap: (index) { //Permite al tocar un boton cambiar de pantalla en la barra de navegación,
+        onTap: (index) {
+          //Permite al tocar un boton cambiar de pantalla en la barra de navegación,
           setState(() {
             _pantallaActual = index;
           });
         },
-        currentIndex: _pantallaActual, //Define la pantalla principal por defecto.
-        items: [ //Items de la barra de navegación.
+        currentIndex:
+            _pantallaActual, //Define la pantalla principal por defecto.
+        items: [
+          //Items de la barra de navegación.
           BottomNavigationBarItem(
               icon: Icon(Icons.home),
               label: "Escuela",
@@ -44,17 +51,19 @@ class ViewInfoSchool extends State<InfoSchool> {
   }
 }
 
-class PantallaHome extends StatelessWidget { //Pantalla de la infomacion de la escuela
+class PantallaHome extends StatelessWidget {
+  //Pantalla de la infomacion de la escuela
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Column(
+      child: ListView(
         children: [
           SizedBox(
             height: 20.0,
           ),
           Text(
-            "Escuela Provincial de Educación Técnica N°20".toUpperCase(), //Titulo principal de la pantalla.
+            "Escuela Provincial de Educación Técnica N°20"
+                .toUpperCase(), //Titulo principal de la pantalla.
             textAlign: TextAlign.center,
             style: TextStyle(
               color: Colors.black,
@@ -68,7 +77,7 @@ class PantallaHome extends StatelessWidget { //Pantalla de la infomacion de la e
           ),
           Text(
             "Centro de educación secundaria", //Subtitulo de la pantalla.
-            textAlign: TextAlign.start,
+            textAlign: TextAlign.center,
             style: TextStyle(
               color: Colors.grey,
               fontSize: 17,
@@ -76,20 +85,173 @@ class PantallaHome extends StatelessWidget { //Pantalla de la infomacion de la e
               fontFamily: 'Impact',
             ),
           ),
-          SizedBox( //Espacio
+          SizedBox(
+            //Espacio
             height: 10.0,
           ),
-          Container( //Imagen de la escuela.
+          Container(
+            //Imagen de la escuela.
             child: Image.asset(
               'assets/epet20.jpg',
               height: 274,
             ),
-            decoration: BoxDecoration(color: Colors.white, boxShadow: [ //Sombra de la imagen.
+            decoration: BoxDecoration(color: Colors.white, boxShadow: [
+              //Sombra de la imagen.
               BoxShadow(
                 offset: Offset(0, 10),
                 blurRadius: 30,
               ),
             ]),
+          ),
+          SizedBox(
+            //Espacio
+            height: 20.0,
+          ),
+          Text(
+            "INFORMACIÓN", //Subtitulo de la pantalla.
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 17,
+              fontWeight: FontWeight.bold,
+              fontFamily: 'Impact',
+            ),
+          ),
+          SizedBox(
+            //Espacio
+            height: 20.0,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.travel_explore_rounded,
+              ),
+              Text(
+                ' http://www.epet20.com.ar/                     ',
+                textAlign: TextAlign.start,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Impact',
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            //Espacio
+            height: 10.0,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.fmd_good_sharp,
+              ),
+              Text(
+                ' Ubicación: Lanín 2020, Q8300 Neuquén',
+                textAlign: TextAlign.start,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Impact',
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            //Espacio
+            height: 10.0,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.gps_fixed,
+              ),
+              Text(
+                ' Coordenadas: 2WP6+3W Neuquén         ',
+                textAlign: TextAlign.start,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Impact',
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            //Espacio
+            height: 10.0,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.phone,
+              ),
+              Text(
+                ' 0299 447-8052                                          ',
+                textAlign: TextAlign.start,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Impact',
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            //Espacio
+            height: 10.0,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.travel_explore_rounded,
+              ),
+              Text(
+                ' http://www.epet20.com.ar/                     ',
+                textAlign: TextAlign.start,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Impact',
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            //Espacio
+            height: 10.0,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.timer_rounded,
+              ),
+              Text(
+                ' 8:00am - 6:30pm                                       ',
+                textAlign: TextAlign.start,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Impact',
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            //Espacio
+            height: 10.0,
           ),
         ],
       ),
@@ -97,7 +259,8 @@ class PantallaHome extends StatelessWidget { //Pantalla de la infomacion de la e
   }
 }
 
-class PantallaFotos extends StatelessWidget { //Pantalla de fotos de la escuela.
+class PantallaFotos extends StatelessWidget {
+  //Pantalla de fotos de la escuela.
   @override
   Widget build(BuildContext context) {
     return Container();
