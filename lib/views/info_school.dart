88ed7d13336948cabb7f5a627b98fdd3c55cca48
Maugit03 <mauricio.benavidez.epet20@gmail.com>
2021-08-16 20:26:39 -0300

    Pantalla Info Escuela - Agregando otra imagen
    Se agrego otra imagen mas a Epet20

--- lib/views/info_school.dart
+++ lib/views/info_school.dart
@@ -288,6 +288,9 @@ class PantallaFotos extends StatelessWidget {
                   Image(
                     image: AssetImage('assets/6fotoepet20.jpg'),
                   ),
+                  Image(
+                    image: AssetImage('assets/7fotoepet20.jpg'),
+                  ),
                 ],
               ),
             ),
