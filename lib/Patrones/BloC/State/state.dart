3ee8c47b6503cd0f89d825a1461faa89a67f77fd
Maugit03 <mauricio.benavidez.epet20@gmail.com>
2021-10-11 10:02:04 -0300

    Panel de control (Drawer) Finalizado
    Se agregó el drawer a la app.

--- lib/Patrones/BloC/State/state.dart
+++ lib/Patrones/BloC/State/state.dart
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
 import 'package:schools_maps/Patrones/BloC/State/state.dart';
-=======
 import 'package:flutter_bloc/flutter_bloc.dart';
-
-
->>>>>>> ad3e89bb7af9eb85c4d1d09d7e4f1eaee1cda9e3
--- lib/Patrones/BloC/bloc.dart
+++ lib/Patrones/BloC/bloc.dart
@@ -1,22 +1,23 @@
-
-<<<<<<< HEAD
 import 'package:schools_maps/Patrones/BloC/Event/event.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'Event/event.dart';
 
 class LoginBloc extends Bloc<LoginEvent, LoginState> {
   LoginBloc(LoginState initialState) : super (initialState) {
-    add(LoadedEvent());
+    //add(LoadedEvent());
   }
 
   @override 
   Stream<LoginState> mapEventToState(LoginEvent event) async* {
     if (event is LoadedEvent) {
       print('Cargando algo');
-      yield
+      //yield()
     }
   }
 }
 
-=======
-import 'package:flutter_bloc/flutter_bloc.dart';
->>>>>>> ad3e89bb7af9eb85c4d1d09d7e4f1eaee1cda9e3
+class LoginState {
+}
+
+class LoadedEvent {
+}
