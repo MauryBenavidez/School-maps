cf74fa8194d47f8b9f0af50e147f581c83013eaa
LeonardoGalindez <88455461+LeonardoGalindez@users.noreply.github.com>
2021-11-09 17:24:40 -0300

    Update login_page.dart

--- lib/UX/login_page.dart
+++ lib/UX/login_page.dart
@@ -245,22 +245,25 @@ class _LoginPageState extends State<LoginPage> {
   }
 
   save() {
-    if (keyForm.currentState!.validate()) {
+    if (keyForm.currentState.validate()) {
       print("Nombre ${nameCtrl.text}");
       print("Correo ${emailCtrl.text}");
-      keyForm.currentState!.reset();
+      keyForm.currentState.reset();
     }
   }
 }
 
 Widget _textFieldName() {
-  var keyboardType2 = null;
   return _textFieldGeneral(
     labelText: "Nombre",
     hintText: "Mauricio Benavidez",
     icon: Icons.person_outline,
     onChanged: (value) {},
+<<<<<<< HEAD
     obscureText: false, errorText: '', keyboardType: keyboardType2,
+=======
+    obscureText: false,
+>>>>>>> f7e0fc927496aa1e0e75071220d1732c88004643
   );
 }
 
@@ -271,18 +274,21 @@ Widget _textFieldEmail() {
     keyboardType: TextInputType.emailAddress,
     onChanged: (value) {},
     icon: Icons.email_outlined,
-    obscureText: false, errorText: '',
+    obscureText: false,
   );
 }
 
 Widget _textFieldPassword() {
-  var keyboardType2 = null;
   return _textFieldGeneral(
     labelText: 'Contraseña',
     onChanged: (value) {},
     icon: Icons.lock_outline_rounded,
     obscureText: true,
+<<<<<<< HEAD
     hintText: '', errorText: '', keyboardType: keyboardType2,
+=======
+    hintText: '',
+>>>>>>> f7e0fc927496aa1e0e75071220d1732c88004643
   );
 }
 
@@ -296,13 +302,13 @@ class _textFieldGeneral extends StatelessWidget {
   final bool obscureText;
   final String errorText;
   const _textFieldGeneral(
-      {required this.labelText,
-      required this.hintText,
-      required this.onChanged,
-      required this.icon,
-      required this.obscureText,
-      required this.keyboardType,
-      required this.errorText});
+      {this.labelText,
+      this.hintText,
+      this.onChanged,
+      this.icon,
+      this.obscureText,
+      this.keyboardType,
+      this.errorText});
 
   @override
   Widget build(BuildContext context) {
