f064f2a57fc350f8adf15aa1bdaf259f2f8131a2
Maugit03 <mauricio.benavidez.epet20@gmail.com>
2021-11-14 15:54:24 -0300

    Pantalla agregar escuelas - Finalizada

--- lib/UI/Pages/views_configuration/add_school.dart
+++ lib/UI/Pages/views_configuration/add_school.dart
@@ -1,8 +1,6 @@
-import 'dart:io';
-
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:image_picker/image_picker.dart';
 
 class Add extends StatefulWidget {
   @override
@@ -10,262 +8,199 @@ class Add extends StatefulWidget {
 }
 
 class AddSchool extends State<Add> {
-  TextEditingController nombreCtrl = new TextEditingController();
-  TextEditingController textoCtrl = new TextEditingController();
-  late File image;
-  Future pickImage(ImageSource source) async {
+  TextEditingController nombre = TextEditingController();
+  TextEditingController direccion = TextEditingController();
+  TextEditingController telefono = TextEditingController();
+  TextEditingController web = TextEditingController();
+  TextEditingController horario = TextEditingController();
+  TextEditingController imagen = TextEditingController();
+  final firebase = FirebaseFirestore.instance;
+  agregar() async {
     try {
-      final image = await ImagePicker().pickImage(source: source);
-      if (image == null) return;
-    } on PlatformException catch (e) {
-      print('Error al seleccionar la imagen: $e');
+      await firebase.collection("escuelas").doc(nombre.text).set({
+        "nombre": nombre.text,
+        "direccion": direccion.text,
+        "telefono": telefono.text,
+        "web": web.text,
+        "horario": horario.text,
+        "imagen": imagen.text,
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  editar() async {
+    try {
+      firebase.collection("escuelas").doc(nombre.text).update({
+        'direccion': direccion.text,
+        'telefono': telefono.text,
+        'web': web.text,
+        'horario': horario.text,
+        'imagen': imagen.text,
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  eliminar() async {
+    try {
+      firebase.collection("escuelas").doc(nombre.text).delete();
+    } catch (e) {
+      print(e);
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Agregar escuela'),
-        backgroundColor: Colors.blue,
-      ),
-      body: new SingleChildScrollView(
-        child: new Container(
-          margin: new EdgeInsets.all(10.0),
-          child: new Form(
-            child: formUI(),
+        appBar: AppBar(
+          title: Text("Agregar escuela"),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: ListView(
+            children: [
+              TextField(
+                controller: nombre,
+                decoration: InputDecoration(
+                    labelText: "Nombre de la escuela",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    )),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              TextField(
+                controller: direccion,
+                decoration: InputDecoration(
+                    labelText: "Dirección",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    )),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              TextField(
+                controller: telefono,
+                decoration: InputDecoration(
+                    labelText: "Telefono",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    )),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              TextField(
+                controller: web,
+                decoration: InputDecoration(
+                    labelText: "Pagina WEB",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    )),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              TextField(
+                controller: horario,
+                decoration: InputDecoration(
+                    labelText: "Horario",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    )),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              TextField(
+                controller: imagen,
+                decoration: InputDecoration(
+                    labelText: "Imagen principal de la escuela",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    )),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                    style: TextButton.styleFrom(backgroundColor: Colors.green),
+                    onPressed: () {
+                      agregar();
+                      nombre.clear();
+                      direccion.clear();
+                      telefono.clear();
+                      web.clear();
+                      horario.clear();
+                      imagen.clear();
+                    },
+                    child: Text('Agregar'),
+                  ),
+                  ElevatedButton(
+                    style: TextButton.styleFrom(backgroundColor: Colors.amber),
+                    onPressed: () {
+                      editar();
+                      nombre.clear();
+                      direccion.clear();
+                      telefono.clear();
+                      web.clear();
+                      horario.clear();
+                      imagen.clear();
+                    },
+                    child: Text('Actualizar'),
+                  ),
+                  ElevatedButton(
+                    style: TextButton.styleFrom(backgroundColor: Colors.red),
+                    onPressed: () {
+                      eliminar();
+                      nombre.clear();
+                      direccion.clear();
+                      telefono.clear();
+                      web.clear();
+                      horario.clear();
+                      imagen.clear();
+                    },
+                    child: Text('Eliminar'),
+                  ),
+                ],
+              ),
+              Container(
+                height: 300,
+                width: double.infinity,
+                child: SingleChildScrollView(
+                  physics: ScrollPhysics(),
+                  child: StreamBuilder<QuerySnapshot>(
+                      stream: firebase.collection('escuelas').snapshots(),
+                      builder: (context, snapshot) {
+                        if (snapshot.hasData) {
+                          return ListView.builder(
+                              shrinkWrap: true,
+                              physics: ScrollPhysics(),
+                              itemCount: snapshot.data!.docs.length,
+                              itemBuilder: (context, i) {
+                                QueryDocumentSnapshot x =
+                                    snapshot.data!.docs[i];
+                                return ListTile(
+                                  title: Text(x['nombre']),
+                                  subtitle: Text(x['direccion']),
+                                );
+                              });
+                        } else {
+                          return Center(child: CircularProgressIndicator());
+                        }
+                      }),
+                ),
+              )
+            ],
           ),
-        ),
-      ),
-    );
-  }
-
-  Widget buildButton(String title, IconData icon, VoidCallback onClicked) =>
-      ElevatedButton(
-        style: ElevatedButton.styleFrom(
-          minimumSize: Size.fromHeight(56),
-          primary: Colors.white,
-          onPrimary: Colors.black,
-          textStyle: TextStyle(fontSize: 20),
-        ),
-        child: Row(
-          children: [
-            Icon(icon, size: 28),
-            const SizedBox(width: 16),
-            Text(title),
-          ],
-        ),
-        onPressed: onClicked,
-      );
-
-  formItemsDesign(
-    icon,
-    item,
-  ) {
-    return Padding(
-      padding: EdgeInsets.symmetric(vertical: 10),
-      child: Card(child: ListTile(leading: Icon(icon), title: item)),
-    );
-  }
-
-  Widget formUI() {
-    return Column(children: <Widget>[
-      SizedBox(
-        height: 20.0,
-      ),
-      Text(
-        "Nombre de escuela".toUpperCase(),
-        textAlign: TextAlign.center,
-        style: TextStyle(
-          color: Colors.black,
-          fontSize: 16,
-          fontWeight: FontWeight.bold,
-          fontFamily: 'Impact',
-        ),
-      ),
-      SizedBox(
-        height: 20.0,
-      ),
-      formItemsDesign(
-          Icons.mode_edit_rounded,
-          TextFormField(
-            controller: nombreCtrl,
-            decoration: new InputDecoration(
-              labelText: 'Nombre de la escuela',
-            ),
-            maxLength: 20,
-          )),
-      SizedBox(
-        height: 20.0,
-      ),
-      Text(
-        "Ubicacion".toUpperCase(),
-        textAlign: TextAlign.center,
-        style: TextStyle(
-          color: Colors.black,
-          fontSize: 16,
-          fontWeight: FontWeight.bold,
-          fontFamily: 'Impact',
-        ),
-      ),
-      SizedBox(
-        height: 20.0,
-      ),
-      formItemsDesign(
-          Icons.fmd_good_sharp,
-          TextFormField(
-            controller: textoCtrl,
-            decoration: new InputDecoration(
-              labelText: 'Ubicación (Calle y dirección)',
-            ),
-            maxLength: 100,
-            keyboardType: TextInputType.text,
-          )),
-      SizedBox(
-        height: 20.0,
-      ),
-      Text(
-        "Coordenadas".toUpperCase(),
-        textAlign: TextAlign.center,
-        style: TextStyle(
-          color: Colors.black,
-          fontSize: 16,
-          fontWeight: FontWeight.bold,
-          fontFamily: 'Impact',
-        ),
-      ),
-      SizedBox(
-        height: 20.0,
-      ),
-      formItemsDesign(
-          Icons.gps_fixed,
-          TextFormField(
-            controller: textoCtrl,
-            decoration: new InputDecoration(
-              labelText: 'Coordenadas',
-            ),
-            maxLength: 100,
-            keyboardType: TextInputType.text,
-          )),
-      SizedBox(
-        height: 20.0,
-      ),
-      Text(
-        "Telefono".toUpperCase(),
-        textAlign: TextAlign.center,
-        style: TextStyle(
-          color: Colors.black,
-          fontSize: 16,
-          fontWeight: FontWeight.bold,
-          fontFamily: 'Impact',
-        ),
-      ),
-      SizedBox(
-        height: 20.0,
-      ),
-      formItemsDesign(
-          Icons.phone,
-          TextFormField(
-            controller: textoCtrl,
-            decoration: new InputDecoration(
-              labelText: 'Numero de telefono',
-            ),
-            maxLength: 100,
-            keyboardType: TextInputType.text,
-          )),
-      SizedBox(
-        height: 20.0,
-      ),
-      Text(
-        "Pagina WEB".toUpperCase(),
-        textAlign: TextAlign.center,
-        style: TextStyle(
-          color: Colors.black,
-          fontSize: 16,
-          fontWeight: FontWeight.bold,
-          fontFamily: 'Impact',
-        ),
-      ),
-      SizedBox(
-        height: 20.0,
-      ),
-      formItemsDesign(
-          Icons.travel_explore_rounded,
-          TextFormField(
-            controller: textoCtrl,
-            decoration: new InputDecoration(
-              labelText: 'URl pagina web',
-            ),
-            maxLength: 100,
-            keyboardType: TextInputType.text,
-          )),
-      SizedBox(
-        height: 20.0,
-      ),
-      Text(
-        "Horario".toUpperCase(),
-        textAlign: TextAlign.center,
-        style: TextStyle(
-          color: Colors.black,
-          fontSize: 16,
-          fontWeight: FontWeight.bold,
-          fontFamily: 'Impact',
-        ),
-      ),
-      SizedBox(
-        height: 20.0,
-      ),
-      formItemsDesign(
-          Icons.timer_rounded,
-          TextFormField(
-            controller: textoCtrl,
-            decoration: new InputDecoration(
-              labelText: 'Horario (Apertura y cierre)',
-            ),
-            maxLength: 100,
-            keyboardType: TextInputType.text,
-          )),
-      SizedBox(
-        height: 20.0,
-      ),
-      Text(
-        "Imagen principal de la escuela".toUpperCase(),
-        textAlign: TextAlign.center,
-        style: TextStyle(
-          color: Colors.black,
-          fontSize: 16,
-          fontWeight: FontWeight.bold,
-          fontFamily: 'Impact',
-        ),
-      ),
-      SizedBox(
-        height: 20.0,
-      ),
-      buildButton('Galeria', Icons.image_outlined,
-          () => {pickImage(ImageSource.gallery)}),
-      SizedBox(
-        height: 15.0,
-      ),
-      GestureDetector(
-          onTap: () {},
-          child: Container(
-            margin: new EdgeInsets.all(30.0),
-            alignment: Alignment.center,
-            decoration: ShapeDecoration(
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30.0)),
-              gradient: LinearGradient(colors: [
-                Color(0xFF0484EC),
-                Color(0xFF0484EC),
-              ], begin: Alignment.topLeft, end: Alignment.bottomRight),
-            ),
-            child: Text("Enviar",
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 18,
-                    fontWeight: FontWeight.w500)),
-            padding: EdgeInsets.only(top: 16, bottom: 16),
-          ))
-    ]);
+        ));
   }
 }
